c7e9f443452328384272f75efeb004b88ffa151d
rameshbalan <balan.ramesh@mavs.uta.edu>
2020-06-16 11:41:07 -0500

    adding week3 partial assignment shell script - command line tools

--- command_line_tools/week_2-command_line_tools_for_gds.sh
+++ command_line_tools/week_2-command_line_tools_for_gds.sh
@@ -20,3 +20,21 @@ echo "How many alignments show the reads's mate mapped to the same chromosome?"
 samtools view athal_wu_0_A_ch*.sam | cut -f 7 | grep -c '='
 echo "How many alignments are spliced?"
 samtools view athal_wu_0_A_ch*.sam | cut -f 6 | grep -c 'N'
+
+echo "How many sequences are there in the genome?"
+samtools view -H athal_wu_0_A.bam | grep -c "SN:"
+echo "What is the length of the first sequence in the genome file?"
+samtools view -H athal_wu_0_A.bam | grep "SN:" |head
+echo "What alignment tool was used?"
+samtools view -H athal_wu_0_A.bam | head -15
+echo "What is the read identifier for the first alignment?"
+
+echo "How many overlaps are reported?"
+bedtools intersect -wo -a athal_wu_0_A_annot.gtf -b athal_wu_0_A.bam | wc -l
+
+echo "How many of these are 10 bases or longer?"
+bedtools intersect -wo -a athal_wu_0_A_annot.gtf -b athal_wu_0_A.bam | rev | cut -f1 | awk '$1>10' | wc -l
+
+echo "Q20"
+bedtools intersect -wo -a athal_wu_0_A_annot.gtf -b athal_wu_0_A.bam |cut -f9 |cut -d ";" -f2 |cut -d ":" -f2 | sort | uniq -c |wc -l
+
---
+++ command_line_tools/week_3-command_line_tools_for_gds.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+# source ~/.zshrc
+# conda activate week2_3
+# wget https://d396qusza40orc.cloudfront.net/gencommand/gencommand_proj3_data.tar.gz
+# tar -xzvf gencommand_proj3_data.tar.gz
+# echo "For Q1-Q5"
+# bowtie2-build wu_0.v7.fas wu_0
+# echo "For Q6-Q14"
+# bowtie2 -x wu_0 -U wu_0_A_wgs.fastq -S wu_0_complete_alignment.sam
+# bowtie2 --local -x wu_0 -U wu_0_A_wgs.fastq -S wu_0_local_alignment.sam
+# echo "For Q15-19"
+
+# echo "For Q20-24"
