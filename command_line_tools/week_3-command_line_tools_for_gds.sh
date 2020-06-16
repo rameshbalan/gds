9704739f70e97673c256698c3d6fb680931fdce5
rameshbalan <balan.ramesh@mavs.uta.edu>
2020-06-16 16:02:52 -0500

    week 3 done - gds - not properly documented

--- command_line_tools/week_3-command_line_tools_for_gds.sh
+++ command_line_tools/week_3-command_line_tools_for_gds.sh
@@ -9,5 +9,10 @@
 # bowtie2 -x wu_0 -U wu_0_A_wgs.fastq -S wu_0_complete_alignment.sam
 # bowtie2 --local -x wu_0 -U wu_0_A_wgs.fastq -S wu_0_local_alignment.sam
 # echo "For Q15-19"
-
+# cut -f6 wu_0_complete_alignment.sam | grep -c -e 'D' -e 'I'
+# cut -f6 wu_0_local_alignment.sam | grep -c -e 'D' -e 'I'
+# cut -f8 wu_uncompressed.vcf| grep "DP" | cut -f1 -d ';' | grep -cE 'DP=([2-9][0-9])'
+# samtools view -S -b wu_0_complete_alignment.sam > wu_0_complete_alignment.bam
+# samtools sort wu_0_complete_alignment.bam -o wu_0_complete_alignment.sorted.bam -O bam -T 
+# wu_0_complete_alignment_sorted
 # echo "For Q20-24"
